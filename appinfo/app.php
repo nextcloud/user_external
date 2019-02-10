104e409e83a2d230fc37cb88a6202ceb3478b73b
Sebastian <sebastian@wiuwiu.de>
2019-02-10 11:24:02 +0100

    Add Prosody XMPP MySQL authentication
    Signed-off-by: Sebastian <sebastian@wiuwiu.de>
    
    Change class name
    
    Signed-off-by: Sebastian <sebastian@wiuwiu.de>
    
    add prosody to title name
    
    Signed-off-by: Sebastian <sebastian@wiuwiu.de>
    
    integrate nextcloud code style guidelines
    
    Signed-off-by: Sebastian <sebastian@wiuwiu.de>
    
    clean up
    
    Signed-off-by: Sebastian <sebastian@wiuwiu.de>
    
    Applied nc code style
    
    Signed-off-by: Sebastian <sebastian@wiuwiu.de>

--- appinfo/app.php
+++ appinfo/app.php
@@ -3,3 +3,4 @@ OC::$CLASSPATH['OC_User_IMAP']='user_external/lib/imap.php';
 OC::$CLASSPATH['OC_User_SMB']='user_external/lib/smb.php';
 OC::$CLASSPATH['OC_User_FTP']='user_external/lib/ftp.php';
 OC::$CLASSPATH['OC_User_BasicAuth']='user_external/lib/basicauth.php';
+OC::$CLASSPATH['OC_User_XMPP']='user_external/lib/xmpp.php';
