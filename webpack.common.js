793182c7271ab2342f8463dd602088563d8b6b68
Jonas Sulzer <jonas@violoncello.ch>
2019-03-09 14:41:42 +0100

    📦 NEW: add npm / webpack / vue build config
    Signed-off-by: Jonas Sulzer <jonas@violoncello.ch>

---
+++ webpack.common.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
+
+module.exports = {
+	entry: path.join(__dirname, 'src', 'main.js'),
+	output: {
+		path: path.resolve(__dirname, './js'),
+		publicPath: '/js/',
+		filename: 'user-external.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.vue$/,
+				loader: 'vue-loader'
+			},
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/
+			}
+		]
+	},
+	plugins: [new VueLoaderPlugin()],
+	resolve: {
+		extensions: ['*', '.js', '.vue', '.json']
+	}
+};
