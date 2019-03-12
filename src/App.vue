1c368bc3c0dc60b66e454494d447626bdc08653f
Jonas Sulzer <jonas@violoncello.ch>
2019-03-12 12:55:09 +0100

    👌 IMPROVE: layout and output format
    Signed-off-by: Jonas Sulzer <jonas@violoncello.ch>

--- src/App.vue
+++ src/App.vue
@@ -19,17 +19,26 @@
 				</thead>
 				<tbody>
 					<tr v-for="(userBackend, key) in userBackends">
-						<td>{{ userBackendNames[key] }}</td>
-						<td><input type="text" name="url" v-bind:value="userBackends[key]['arguments'][0]" placeholder="URL"></td>
-						<td><input v-if="userBackendNames[key] === 'OC_User_IMAP'" type="text" name="domain" v-bind:value="userBackends[key]['arguments'][1]" placeholder=""></td>
-						<td></td>
+						<td>{{ backendNamesReadable[key] }}</td>
+						<td>
+							<input type="text" name="url" v-bind:value="userBackends[key]['arguments'][0]" placeholder="URL">
+						</td>
+						<td>
+							<span v-if="userBackendNames[key] === 'OC_User_IMAP'">Restrict login to a specific e-mail domain:<br>
+								<input type="text" name="domain" v-bind:value="userBackends[key]['arguments'][1]" placeholder="">
+							</span>
+							<span v-if="userBackendNames[key] === 'OC_User_FTP'" id="centertext">
+								<input type="checkbox" id="ftps" class="added" value="true" v-model="userBackends[key]['arguments'][1]">Use secure ftps instead of plain ftp.
+							</span>
+						</td>
+						<td class="save"><div class="icon-checkmark" title="Save"></div></td>
 					</tr>
 					<tr>
 						<td>
 							<select id="selectBackend" class="selectBackend">
 								<option value="" disabled="" selected="" style="display:none;">Add External User Backend</option>
 								<option value="imap">IMAP</option>
-								<option value="ftp">FTP</option>
+								<option value="ftp">FTP(S)</option>
 								<option value="smb">SMB/Samba</option>
 								<option value="basic_auth">HTTP(S) Basic Auth</option>
 								<option value="dav">WebDAV</option>
@@ -74,6 +83,56 @@ export default {
 		},
 		userBackends() {
 			return this.serverData.user_backends;
+		},
+		backendNamesShort() {
+			this.backendsShort = [];
+			for (var i in this.userBackendNames) {
+				switch (this.userBackendNames[i]) {
+				  case "OC_User_FTP":
+				    this.backendsShort.push("ftp");
+				    break;
+				  case "OC_User_IMAP":
+				    this.backendsShort.push("imap");
+				    break;
+				  case "OC_User_SMB":
+				    this.backendsShort.push("smb");
+				    break;
+				  case "\OCA\User_External\WebDAVAuth":
+				    this.backendsShort.push("dav");
+				    break;
+				  case "OC_User_BasicAuth":
+				    this.backendsShort.push("basic_auth");
+						break;
+					default:
+						this.backendsShort.push("unknown");
+				}
+			}
+			return this.backendsShort;
+		},
+		backendNamesReadable() {
+			this.backendsReadable = [];
+			for (var i in this.backendNamesShort) {
+				switch (this.backendNamesShort[i]) {
+				  case "ftp":
+				    this.backendsReadable.push("FTP(S)");
+				    break;
+				  case "imap":
+				    this.backendsReadable.push("IMAP");
+				    break;
+				  case "smb":
+				    this.backendsReadable.push("SMB/Samba");
+				    break;
+				  case "dav":
+				    this.backendsReadable.push("WebDAV");
+				    break;
+				  case "basic_auth":
+				    this.backendsReadable.push("HTTP(S) Basic Auth");
+						break;
+					default:
+						this.backendsReadable.push("unknown");
+				}
+			}
+			return this.backendsReadable;
 		}
 
 	},
