0cd0e599010cf8941742a66e15b38cf9b807e008
Michael E. Schindler <mail@msfuture.de>
2021-05-26 11:28:44 +0200

    Switch to IClient wrapper for performing the request.
    Signed-off-by: Michael E. Schindler <mail@msfuture.de>

--- lib/rest.php
+++ lib/rest.php
@@ -43,6 +43,8 @@
  *
  */
 
+use OCP\AppFramework\Http;
+
 class OC_User_REST extends \OCA\user_external\Base {
 
 	private $authUrl;
@@ -60,27 +62,40 @@ class OC_User_REST extends \OCA\user_external\Base {
 	 * @param string $uid      The username
 	 * @param string $password The password
 	 *
-	 * @return true/false
+	 * @return string $uid      The username
 	 */
 	public function checkPassword($uid, $password) {
 		$postdata = json_encode(array("user"=>array("id"=>$uid,"password"=>$password)));
-		$ch = curl_init();
-		$headers = ['Content-Type: application/json'];
-		curl_setopt($ch,CURLOPT_URL,$this->authUrl."/_nextcloud/user_external/v1/authenticate");
-		curl_setopt($ch, CURLOPT_POST, true);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_HTTPHEADER,$headers);
-		$result = curl_exec($ch);
-		$statusCode = curl_getinfo($ch,CURLINFO_RESPONSE_CODE);
 
-        $result = json_decode($result,true);
-        if($statusCode==="404") {
-            OC::$server->getLogger()->error(
-                'ERROR: REST Server sent 404 on: '.$this->authUrl,
-                ['app' => 'user_external']
-            );
-        } elseif(is_array($result)) {
+		$client = \OC::$server->getHTTPClientService()->newClient();
+		try {
+		    $response = $client->post($this->authUrl."/_nextcloud/user_external/v1/authenticate", [
+		       'body' => $postdata,
+               'timeout' => 10,
+               'connect_timeout' => 10,
+            ]);
+            $result = json_decode($response->getBody(),true);
+        } catch (\GuzzleHttp\Exception\RequestException $e) {
+		    if($e->getCode() === Http::STATUS_UNAUTHORIZED || $e->getCode() === Http::STATUS_FORBIDDEN) {
+                OC::$server->getLogger()->error(
+                    'ERROR: Resource on REST server is forbidden on: '.$this->authUrl,
+                    ['app' => 'user_external']
+                );
+            } elseif($e->getCode() === Http::STATUS_NOT_FOUND) {
+                OC::$server->getLogger()->error(
+                    'ERROR: REST Server sent 404 on: '.$this->authUrl,
+                    ['app' => 'user_external']
+                );
+            } else {
+                OC::$server->getLogger()->error(
+                    'ERROR: Unknown request error on: '.$this->authUrl,
+                    ['app' => 'user_external']
+                );
+            }
+		    $result = false;
+        }
+
+        if(is_array($result)) {
             if($result['auth']['success']===true) {
                 $user_exists = $this->userExists($result['auth']['id']);
                 $uid = $this->storeUser($result['auth']['id'],$result['auth']['groups']);
